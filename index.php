9487a0adef8b2f9ca0afd8427c7b525a302887e7
Choquinn <voltzchoquinn@gmail.com>
2025-05-30 12:29:55 -0300

    Deletei um link de css, corrigi uma palavra

--- index.php
+++ index.php
@@ -7,8 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Reserva de Salas</title>
-
-    <link rel="stylesheet" href="estilos.css" />
+    
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -74,7 +73,7 @@
                         <th class="w-2/5 py-2 px-4 bg-gray-200 text-center text-xs font-semibold text-gray-700 uppercase tracking-wider">Descrição</th>
                         <th class="w-1/5 py-2 px-4 bg-gray-200 text-center text-xs font-semibold text-gray-700 uppercase tracking-wider">Reservado por</th>
                         <th class="w-1/5 py-2 px-4 bg-gray-200 text-center text-xs font-semibold text-gray-700 uppercase tracking-wider">Data e Hora</th>
-                        <th class="w-1/5 py-2 px-4 bg-gray-200 text-center text-xs font-semibold text-gray-700 uppercase tracking-wider">ações</th>
+                        <th class="w-1/5 py-2 px-4 bg-gray-200 text-center text-xs font-semibold text-gray-700 uppercase tracking-wider">Ações</th>
                     </tr>
                 </thead>
                 <tbody>
